25274619791c9c0dc4bbc5c44c088455c9dc0bfc
alex1231laurin-byte <alex1231.laurin@gmail.com>
2025-10-26 11:12:47 -0400

    question 30 part 3
    Refactor state update logic to use separate variables for left and center cells.

--- Automaton.java
+++ Automaton.java
@@ -50,6 +50,8 @@ public class Automaton
         int[] nextState = new int[state.length];
         // Naively update the state of each cell
         // based on the state of its two neighbors.
+        int left = 0;
+        int center = state[0];
         for(int i = 0; i < state.length; i++) {
             int right = i + 1 < state.length ? state[i + 1] : 0;
             nextState[i] = (left + center + right) % 2;
